f38e6d20d4a58407e1fceca6e76335c573977fa5
Michael <76599155+grayoctagon@users.noreply.github.com>
2025-07-06 00:22:40 +0200

    +comment

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Author: Michael Beck
+ * 
+ * Repository: https://github.com/grayoctagon/NetterWetter
+ * 
+ * *developed with AI support*
+ * 
+ */
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
